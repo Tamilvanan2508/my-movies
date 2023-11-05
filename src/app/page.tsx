605dd5da4257c3fe14e41b8d5abe0a219aa82f21
Tamilvanan <tamilvanan@doodleblue.com>
2023-11-05 20:59:07 +0530

    pages changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import { NextPage } from "next";
+
+const Home: NextPage = () => {
+  const greeting: string = "Welcome to Movies now";
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Welcome to, Movies now.
+      <h1>{greeting}</h1>
     </main>
   );
-}
+};
+
+export default Home;
