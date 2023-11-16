f06785fc4c7ec67bfdec822c7f77856d1860ecd7
Tamilvanan <tamilvanan@doodleblue.com>
2023-11-16 21:52:34 +0530

    chanegs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   const greeting: string = "Welcome to Movies now";
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>{greeting}</h1>
     </main>
   );
 };
+
 export default Home;
